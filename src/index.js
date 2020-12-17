69a40c69c7b05b39f0edb0952d7a7cfca64afb1a
GourishPadihar <63400249+GourishPadihar@users.noreply.github.com>
2020-12-17 14:31:30 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,73 +9,127 @@ app.use(express.json());
 const { connection } = require('./connector')
 
 
-app.get('/totalRecovered',(request, response)=>{
-
-    connection.find().then(data=>{
-        let totalRecovered = 0;
-        data.forEach(state=> totalRecovered+= (state.recovered));
-        response.send( {data: {_id: "total", recovered: totalRecovered}});
-    })
-});
-
-app.get('/totalActive',(request, response)=>{
-
-    connection.find().then(data=>{
-        let totalInfected = 0;
-        let totalRecovered = 0;
-        data.forEach(state=>{
-            totalRecovered += state.recovered;
-            totalInfected += state.infected;
-        })
-        response.send( {data: {_id: "total", active: totalInfected - totalRecovered}});
-    });
-});
-
-app.get('/totalDeath',(request, response)=>{
-
-    connection.find().then(data=>{
-        let totalDeath = 0;
-        data.forEach(state=> totalDeath += state.death);
-        response.send( {data: {_id: "total", death: totalDeath}});
-    });
-});
-
-app.get('/hotspotStates',(request, response)=>{
-
-    connection.find().then(data=>{
-        let hotspot = [];
-        data.forEach(state=>{
-            const rate = ((state.infected-state.recovered) / state.infected).toFixed(5);
-            if(rate > 0.1){
-                hotspot.push({
-                    state: state.state,
-                    rate: rate
-                });
+app.get("/totalRecovered",  async(req,res)=>{
+    const totalRecovered =  await collection_connection.aggregate([ {
+        $group: {
+           _id: "total",
+            "recovered": {
+                $sum:"$recovered"
             }
-        });
-        response.send( {data: hotspot});
-    });
+        }
+    } ] );
+    let x = totalRecovered[0];
+    let y = {...x};
+    let result = {data: y};
+    res.send(result);
+})
 
-});
+app.get("/totalActive", async(req,res)=>{
+    const recovered =  await collection_connection.aggregate([ {
+        $group: {
+            _id: "total",
+            "recovered": {
+                $sum:"$recovered"
+            }
+        }
+    } ] );
+    //console.log(recovered);
+    const recover = recovered[0].recovered;
 
+    const infected =  await collection_connection.aggregate([ {
+        $group: {
+            _id: "total",
+            "infected": {
+                $sum:"$infected"
+            }
+        }
+    } ] );
+    const infect = infected[0].infected;
 
+    const active = infect - recover;
 
-app.get('/healthyStates',(request, response)=>{
+    const result = {data: {_id: "total", active:active}};
+
+    res.send(result);
+})
+
+app.get("/totalDeath", async (req,res)=>{
+
+    const deathToll =  await collection_connection.aggregate([ {
+        $group: {
+            _id: "total",
+            "death": {
+                $sum:"$death"
+            }
+        }
+    } ] );
+
+    const x= deathToll[0];
+    const result = {data : x};
+    console.log(result);
+
+    res.send(result);
+})
+
+app.get("/hotspotStates", async (req,res)=>{
+
+    const data = await collection_connection.aggregate(
+        [
+            {
+                $project:{
+                    _id:0,
+                    state: 1,
+                    rate: {                       
+                        $round:   [{$divide: [{ $subtract: [ "$infected", "$recovered" ] }, "$infected"]}, 5]
+                    }
+                }
+            }
+        ]
+    )
+
+    const newData = [];
+
+    for(let i=0;i<data.length;i++){
+        if(data[i].rate> 0.1){
+            newData.push(data[i]);
+        }
+    }
+
+    //console.log(NewData);
+    const result = {data: newData};
+    res.send(result);
+})
+
+app.get("/healthyStates", async (req,res)=>{
+
+    const data = await collection_connection.aggregate(
+        [
+            {
+                $project:{
+                    _id:0,
+                    state: 1,
+                    mortality: {                       
+                        $round:   [{ $divide: [ "$death", "$infected" ] }, 5]
+                    }
+                }
+            }
+        ]
+    )
+
+    const newData = [];
+
+    for(let i=0;i<data.length;i++){
+        if(data[i].mortality<  0.005){
+            newData.push(data[i]);
+        }
+    }
+    const result = {data: newData};
     
-    connection.find().then(data=>{
-        let healthy = [];
-        data.forEach(state=>{
-            const morality = (state.death / state.infected).toFixed(5);
-            if(morality < 0.005){
-                healthy.push({
-                    state: state.state,
-                    morality: morality
-                });
-            }
-        });
-        response.send( {data: healthy});
-    });
-});
+    res.send(result);
+})
+
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
+
 module.exports = app;
